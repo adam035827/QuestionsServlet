36359d21b9bc3b2a94d0d4ac99f4ec0a5b8fb76e
Adam B <adam035827@gmail.com>
2016-11-22 17:43:47 -0600

    Refactor

--- src/AnswerServlet.java
+++ src/AnswerServlet.java
@@ -1,32 +1,19 @@
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AnswerServlet
- */
 @WebServlet("/AnswerServlet")
 public class AnswerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public AnswerServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		    response.setContentType("text/html");
 		    int questionNum = Integer.parseInt(request.getParameter("question"));
@@ -41,7 +28,6 @@ public class AnswerServlet extends HttpServlet {
 		    out.println("<body>");
 		    out.println("<h1>Answers</h1>");
 		    out.println("<h2>" + db.getQuestion(questionNum) + "</h1>");
-		    
 		    out.println("<form action=\"AnswerServlet?question=" + questionNum + "\" method=\"GET\">");
 		    out.println("<input type=\"text\" name=\"newAnswer\" />");
 		    out.println("<input type=\"hidden\" name=\"question\" value="+ questionNum + ">");
@@ -60,7 +46,6 @@ public class AnswerServlet extends HttpServlet {
 		
 		    String newAnswer = request.getParameter("newAnswer");
 		    
-		    
 		    if (newAnswer != null){
 		    	if (answerCount > 1){
 		    		if (!newAnswer.equals(answers[answerCount-2])){
@@ -78,11 +63,7 @@ public class AnswerServlet extends HttpServlet {
 		    newAnswer = null;
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- src/QuestionServlet.java
+++ src/QuestionServlet.java
@@ -1,31 +1,18 @@
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class QuestionServlet
- */
 @WebServlet("/QuestionServlet")
 public class QuestionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public QuestionServlet() {
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		    response.setContentType("text/html");
 		    PrintWriter out = response.getWriter();
@@ -33,13 +20,11 @@ public class QuestionServlet extends HttpServlet {
 		    DBAbstractionLayer db = new DBAbstractionLayer();
 		    
 		    out.println("<html>");
-		    
 		    out.println("<head>");
 		    out.println("<title>Question Servlet</title>");
 		    out.println("</head>");
 		    out.println("<body>");
 		    out.println("<h1> Questions</h1>");
-		    
 		    out.println("<form action=\"QuestionServlet\" method=\"GET\">");
 		    out.println("<input type=\"text\" name=\"newQuestion\" />");
 		    out.println("<input type=\"submit\" value=\"Add Question\" />");
@@ -68,11 +53,7 @@ public class QuestionServlet extends HttpServlet {
 		    newQuestion = null;
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
